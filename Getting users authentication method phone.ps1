114cb135d601dc9b905f46e6f2ff11d38849a594
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2023-03-10 14:56:47 +0000

    Add files via upload

---
+++ Getting users authentication method phone.ps1
@@ -0,0 +1,31 @@
+﻿
+Connect-MgGraph
+
+Select-MgProfile -Name "beta"
+
+$userId = "user obj id"
+
+$userAuthMethod = Get-MgUserAuthenticationMethod -UserId $userId
+
+
+#https://learn.microsoft.com/en-us/graph/api/resources/authenticationstrengths-overview?view=graph-rest-beta&viewFallbackFrom=graph-rest-1.0
+#https://learn.microsoft.com/en-us/graph/api/resources/user?view=graph-rest-1.0
+
+$userAuthMethod = Get-MgUserAuthenticationPhoneMethod -UserId $userId
+#https://learn.microsoft.com/en-us/graph/api/authentication-list-phonemethods?view=graph-rest-beta&tabs=powershell (good for a use)
+
+# so can right a script to get all users then use the id to check if the have a phone number set 
+
+# and can use this to set one
+$params = @{
+	PhoneNumber = "+1 2065555554"
+	PhoneType = "mobile"
+}
+
+Update-MgUserAuthenticationPhoneMethod -UserId $userId -PhoneAuthenticationMethodId $phoneAuthenticationMethodId -BodyParameter $params
+
+#https://learn.microsoft.com/en-us/graph/api/phoneauthenticationmethod-update?view=graph-rest-beta&tabs=powershell
+
+
+#use this command to filter user with mobile phone as default MFA method
+Get-MgReportAuthenticationMethodUserRegistrationDetail -all
